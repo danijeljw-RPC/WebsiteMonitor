5eb7ddac78e8b9351a822366a74e3acdeb874849
Danijel Wynyard <danijel@repasscloud.com>
2025-11-26 22:32:24 +1100

    update build script for testing (dry-run)

--- build.ps1
+++ build.ps1
@@ -6,3 +6,9 @@ Remove-Item -Path $PSScriptRoot\bin -Recurse -Force -Confirm:$false -ErrorAction
 Remove-Item -Path $PSScriptRoot\obj -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue
 
 dotnet publish .\src\WebsiteMonitor.csproj -c Release -r win-x64 /p:PublishAot=true -o .\publish\win-x64
+
+Set-Location -Path $PSScriptRoot\publish\win-x64
+
+.\WebsiteMonitor.exe --generate-yaml-config
+
+.\WebsiteMonitor.exe
